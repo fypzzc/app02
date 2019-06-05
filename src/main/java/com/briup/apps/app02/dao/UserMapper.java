44b431d7af4142b2a67d6323adddeba39e2b0b91
fypzzc <1471116098@qq.com>
2019-06-05 20:05:39 +0800

    zheshiyicitijiao

---
+++ src/main/java/com/briup/apps/app02/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.briup.apps.app02.dao;
+
+
+import com.briup.apps.app02.bean.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    List<User> selectAll();
+    User selectByPrimaryKey(long id);
+
+    void insert(User user);
+    void update(User user);
+    void deleteByPrimaryKey(long id);
+
+}
